d2b1c042aa52f2e869a81fd5e238fccd99164b1d
Selfcommit <oboyledk@gmail.com>
2012-06-12 12:12:45 -0400

    commented basic functions

---
+++ classes/class.database.php
@@ -0,0 +1,5 @@
+<?php
+$username="phpdhcp"
+$database="phpdhcp"
+$password="YAG6e2RWmJdXJz5B"
+?>
--- classes/class.ipaddresses.php
+++ classes/class.ipaddresses.php
@@ -1,1 +1,39 @@
-<?php echo "IP addressesing class"; ?>
+<?php echo "IP addressesing class"; 
+
+
+function Assign_IP ($mac,$group) 	{
+//Takes in the mac address of a device to be added
+
+//access the Database
+
+//checks MAC address against existing mac addresses in IP_addresses Table
+
+//If MAC is unique look for next available IP according to $group
+
+//assign next available IP to device
+
+//return IP address
+							}
+							
+function Remove_IP ($mac) 	{
+//Takes in a mac address of a device to be added
+
+//accesses the Database
+
+//Checks for MAC address in IP_Addresses Table
+
+//If Found, clears device ID for the IP address
+
+							}
+
+function rebuild_DHCP() {
+//renames /etc/dhcp/dhcpd.conf to /etc/dhcp/dhcpd.conf_old
+
+//rebuilds DHCP file into a variable based on DB information
+//Builds entry only if IP address has a DeviceID
+
+//writes variable into file_put_contents(/etc/dhcp/dhcpd.conf)
+
+//restarts dhcp service
+						}							
+?>
